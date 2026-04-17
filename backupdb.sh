ad15b44702779781c258a4488af029887dc946e7
Alexander Gent <aagent298@gmail.com>
2026-04-17 14:33:38 +1000

    Update backupdb.sh

--- backupdb.sh
+++ backupdb.sh
@@ -12,5 +12,5 @@ echo '.var/app/tv.kodi.Kodi/data/userdata/keymaps/keyboard.xml'>>/tmp/file.lst
 
 ## Backup User Profile...
 echo -e "\033[1;33mUser \"$currentuser\"... \033[1;34mBackup User Profile\033[0m"
-tar --exclude-from=/tmp/file.lst -zcvf profile-backup.tar.gz --absolute-names -C '/home/'$currentuser'/' '.config/mozilla' '.var/app/tv.kodi.Kodi'
+tar --exclude-from=/tmp/file.lst -zcvf profile-backup.tar.gz --absolute-names -C '/home/'$currentuser'/' '.mozilla' '.var/app/tv.kodi.Kodi'
 rm -f /tmp/file.lst
