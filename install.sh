c6cad3befe7c5f2bc2c698db9d60203b96715f6c
Alexander Gent <aagent298@gmail.com>
2026-04-20 12:47:04 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ fi
 }
 
 function get-kodi() {
-echo -e '\033[1;33mInstalling \033[1;34mKODI Media Centre\033[0m'
+echo -e '\033c\033[1;33mInstalling \033[1;34mKODI Media Centre\033[0m'
 mkdir -p /var/lib/flatpak/exports/share
 mkdir -p /root/.local/share/flatpak/exports/share
 install-package flatpak
@@ -172,7 +172,7 @@ cp -f /usr/share/applications/org.gnome.Mahjongg.desktop /home/$currentuser/Desk
 }
 
 function get-php() {
-echo -e '\033[1;33mInstalling \033[1;34mApache\033[0m'
+echo -e '\033c\033[1;33mInstalling \033[1;34mApache\033[0m'
 file="/etc/apt/sources.list.d/php.list"
 if [ -f "$file" ] ; then
     rm -f "$file"
